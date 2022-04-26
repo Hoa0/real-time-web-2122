5af629e524270b44886f86f549f5c7d1613cd364
hoa0 <t-hoa.l@hotmail.com>
2022-04-26 11:08:30 +0200

    📦️ install node fetch

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ https://socket.io/get-started/chat
 
 const express = require('express')
 const app = express()
+const fetch = require('node-fetch')
 const http = require('http').createServer(app)
 const path = require('path')
 const io = require('socket.io')(http)
-const port = process.env.PORT || 4242
+const PORT = process.env.PORT || 4242
 
 // Template files
 app.set('views', 'views');
@@ -17,12 +18,33 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.resolve('static')))
 
-
+const endpoint= 'https://dog.ceo/api/breeds/list/all'
+/*
+app.get('/', (req, res) => {
+    fetch(`${endpoint}`)
+        .then(async response => {
+            const artCollection = await response.json()
+            res.render('index', {
+                title: 'Art overview',
+                titlePage: 'Artcollection',
+                data: artCollection.artObjects
+            })
+        })
+        .catch(err => res.send(err))
+})*/
 // index route
 app.get('/', (req, res) => {
+    fetch(endpoint)
+    .then(async response =>{
+        const collection = await response.json()
         res.render('index', {
         pageTitle: 'Chat',
-    });
+        data:collection.message
+        
+    })
+    console.log("check test",collection);
+    })
+    .catch(err => res.send(err))
 })
 
 io.on('connection', (socket) => {
@@ -37,6 +59,6 @@ io.on('connection', (socket) => {
     })
 })
 
-http.listen(port, () => {
-    console.log('listening on port ', port)
+http.listen(PORT, () => {
+    console.log('listening on port ', PORT)
 })
